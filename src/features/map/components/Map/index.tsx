255362826a691ec74459ad2da6ab01181ca2bd8a
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-08-31 16:16:28 -0700

    Revert to simpler map loader cutsom hook

---
+++ src/features/map/components/Map/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import useMapLoader from "../../hooks/useMapLoader";
+
+export default function Map() {
+  useMapLoader();
+
+  return <section className="h-full" id="map" />;
+}
--- src/features/map/hooks/useMapLoader.ts
+++ src/features/map/hooks/useMapLoader.ts
@@ -1,10 +1,19 @@
-import { useEffect } from "react";
 import { Loader } from "@googlemaps/js-api-loader";
+import { useEffect } from "react";
 
 const loader = new Loader({
-  apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
   version: "weekly",
 });
+
+const mapOptions = {
+  center: { lat: 34.052, lng: -118.243 },
+  zoom: 10,
+  mapId: process.env.NEXT_PROCESS_MAP_ID,
+  mapTypeControl: false,
+  streetViewControl: false,
+  fullscreenControl: false,
+};
 
 export default function useMapLoader() {
   useEffect(() => {
@@ -12,15 +21,10 @@ export default function useMapLoader() {
       const { Map } = (await google.maps.importLibrary(
         "maps",
       )) as google.maps.MapsLibrary;
-
-      let map = new Map(document.getElementById("map") as HTMLElement, {
-        center: { lat: 34.052, lng: -118.243 },
-        zoom: 10,
-        mapId: process.env.NEXT_PROCESS_MAP_ID,
-        mapTypeControl: false,
-        streetViewControl: false,
-        fullscreenControl: false,
-      });
+      let map = new Map(
+        document.getElementById("map") as HTMLElement,
+        mapOptions,
+      );
     });
   });
 }
